a4f22a73edc471685326a32343396829fd361210
Lauri Leppanen <lepplaju@ws-c3313-17.it.jyu.fi>
2026-05-18 16:50:19 +0300

    improve scroll behaviour

--- timApp/modules/chattim/client/chattim.ts
+++ timApp/modules/chattim/client/chattim.ts
@@ -103,27 +103,29 @@ export interface ControlPanelData extends ControlPanelSettings {
             <ng-container header> {{ header }}</ng-container>
             <ng-container body>
                 <div class="chattim-body scroll-box">
-                    <div class="scroll-box" #conversationScroll>
-                        <div *ngIf="conversation.length === 0" class="chat-welcome">
-                            <ng-container *ngIf="markup.welcomeText; else localizedWelcome">
-                                {{ markup.welcomeText }}
-                            </ng-container>
-                            <ng-template #localizedWelcome>
-                                <span i18n>"Welcome to use TIM's helper chatbot!"</span>
-                            </ng-template>
+                    <div class="upper-area">
+                        <div class="" #conversationScroll>
+                            <div *ngIf="conversation.length === 0" class="chat-welcome">
+                                <ng-container *ngIf="markup.welcomeText; else localizedWelcome">
+                                    {{ markup.welcomeText }}
+                                </ng-container>
+                                <ng-template #localizedWelcome>
+                                    <span i18n>"Welcome to use TIM's helper chatbot!"</span>
+                                </ng-template>
+                            </div>
+                            <div *ngFor="let entry of conversation">
+                                <div class="chat-user">{{ entry.user.content }}</div>
+                                <div class="chat-bot" [innerHTML]="entry.agent.content | purify"></div>
+                            </div>
                         </div>
-                        <div *ngFor="let entry of conversation">
-                            <div class="chat-user">{{ entry.user.content }}</div>
-                            <div class="chat-bot" [innerHTML]="entry.agent.content | purify"></div>
-                        </div>
-                    </div>
 
-                    <div>
-                        <tim-loading *ngIf="isRunning"></tim-loading>
-                        <div *ngIf="error" [innerHTML]="error | purify"></div>
-                    </div>
-                    <label class="justify-center w-100">{{ inputStem }} </label>
-                    <div class="d-flex flex-row w-100 justify-content-center chat-row">
+                        <div>
+                            <tim-loading *ngIf="isRunning"></tim-loading>
+                            <div *ngIf="error" [innerHTML]="error | purify"></div>
+                        </div>
+                        <label class="justify-center w-100">{{ inputStem }} </label>
+                        <div class="sticky-chat-row">
+                            <div class="d-flex flex-row w-100 justify-content-center chat-row">
                     <textarea i18n-placeholder class="form-control chat-textarea"
                               rows="2"
                               placeholder="Ask me about TIM related things"
@@ -133,12 +135,14 @@ export interface ControlPanelData extends ControlPanelSettings {
                               oninput="this.style.height='auto'; this.style.height=this.scrollHeight+'px'">
                     </textarea>
 
-                        <button class="timButton flex-shrink-0 ms-2"
-                                *ngIf="buttonText()"
-                                [disabled]="!canSendInput()"
-                                (click)="sendUserInput()"
-                                [innerHTML]="buttonText() | purify">
-                        </button>
+                                <button class="timButton flex-shrink-0 ms-2"
+                                        *ngIf="buttonText()"
+                                        [disabled]="!canSendInput()"
+                                        (click)="sendUserInput()"
+                                        [innerHTML]="buttonText() | purify">
+                                </button>
+                            </div>
+                        </div>
                     </div>
                     <div class="control-panel-container">
                         <chattim-control-panel
